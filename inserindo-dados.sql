1851aa99ffeccaacd5c26e43e6a2be96bb272808
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-27 14:57:13 -0200

    Atualição dos scripts de inserção.

--- inserindo-dados.sql
+++ inserindo-dados.sql
@@ -7,5 +7,6 @@ VALUES
 INSERT INTO FUN_FUNCIONARIOS
 (FUN_NOME, FUN_SOBRENOME, FUN_DATA_NASCIMENTO, FUN_OBSERVACOES)
 VALUES
-('Maria', 'Aparecida da Silva', '10/06/1980', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-('João', 'Batista da Silva', '23/02/1888', 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+('João', 'Batista da Silva', '23/02/1888', 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+('Isabela', 'Souza', '20/01/1990', ''),
+('Victoria', 'Moreira', '11/03/1880', '');
