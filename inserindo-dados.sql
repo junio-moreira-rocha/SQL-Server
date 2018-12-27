e0bdeab3fe2c3c0856d17a5734d41d3c526beb90
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-27 13:03:21 -0200

    Adicioando script com exemplo de insert para multiplas linhas.

--- inserindo-dados.sql
+++ inserindo-dados.sql
@@ -1,3 +1,11 @@
 -- Inserindo Dados:
-INSERT INTO FUN_FUNCIONARIOS(FUN_NOME, FUN_SOBRENOME, FUN_DATA_NASCIMENTO, FUN_OBSERVACOES)
-VALUES('Maria', 'Aparecida da Silva', '10/06/1980', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+INSERT INTO [MeuRH].[dbo].[FUN_FUNCIONARIOS]
+([FUN_NOME], [FUN_SOBRENOME], [FUN_DATA_NASCIMENTO], [FUN_OBSERVACOES])
+VALUES
+('Maria', 'Aparecida da Silva', '10/06/1980', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+-- ou inserindo varias linhas:
+INSERT INTO FUN_FUNCIONARIOS
+(FUN_NOME, FUN_SOBRENOME, FUN_DATA_NASCIMENTO, FUN_OBSERVACOES)
+VALUES
+('Maria', 'Aparecida da Silva', '10/06/1980', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+('João', 'Batista da Silva', '23/02/1888', 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
