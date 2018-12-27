cbd3144d17e714bd3d01ccd75cf19d3ef3d6a071
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-27 14:55:44 -0200

    Scripts com exemplos de como usar o Collate.

---
+++ collate.sql
@@ -0,0 +1,7 @@
+-- Collate:
+-- Usado para modificar momentaneamente como o banco de dados interpreta os caracteres, CI/CS (Case Insensitive/Sensitive), AI/AS (Accent Insensitive/Sensitive):
+SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_NOME = 'JoAo' COLLATE Latin1_General_CI_AI;
+--
+-- Função que exibe os Collates:
+SELECT NAME, DESCRIPTION
+FROM fn_helpcollations();
