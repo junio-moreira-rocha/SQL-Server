75c6c208aa1f6498cec75e32c14abc2dbab837ff
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-27 13:56:32 -0200

    Adicionado titulo as consultas.

--- consutlas-basicas.sql
+++ consutlas-basicas.sql
@@ -1,7 +1,19 @@
 -- Consultas Básicas:
+--
+-- Operador de igualdade:
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_NOME = 'Maria';
+--
+-- Operador de diferente:
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_NOME <> 'Maria';
+--
+-- Operador "menor que":
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_ID < 100;
+--
+-- Operador "maior que":
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_DATA_NASCIMENTO > '01/01/2018';
+--
+-- Operador "menor ou igual a":
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_ID <= 100;
+--
+-- Operador "maior ou igual a":
 SELECT * FROM FUN_FUNCIONARIOS WHERE FUN_DATA_NASCIMENTO >= '01/01/2018';
