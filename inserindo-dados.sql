6c26345a5ddbd84eedd4b4322514621fcc577e99
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2018-12-27 10:21:21 -0200

    Script para inserir dados.

---
+++ inserindo-dados.sql
@@ -0,0 +1,3 @@
+-- Inserindo Dados:
+INSERT INTO FUN_FUNCIONARIOS(FUN_NOME, FUN_SOBRENOME, FUN_DATA_NASCIMENTO, FUN_OBSERVACOES)
+VALUES('Maria', 'Aparecida da Silva', '10/06/1980', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
